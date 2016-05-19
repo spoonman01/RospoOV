0bbd15d16b8e9099f2c4c448a9d1e40f0cab7ba8
spoonman01 <luca.rospocher@studenti.unitn.it>
2016-05-19 17:21:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $update = json_decode(file_get_contents('php://input'));
 //your app
 try {
 
-    if($update->message->text == '/email' || '/email@RospoOV_Bot')
+    if($update->message->text == '/email' || $update->message->text == '/email@RospoOV_Bot')
     {
     	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
     	$response = $client->sendMessage([
@@ -34,7 +34,7 @@ try {
         	'text' => "You can send email to : lucarospocher@gmail.com"
      	]);
     }
-    else if($update->message->text == '/help' || '/help@RospoOV_Bot')
+    else if($update->message->text == '/help' || $update->message->text == '/help@RospoOV_Bot')
     {
     	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
     	$response = $client->sendMessage([
@@ -43,7 +43,7 @@ try {
     		]);
 
     }
-    else if($update->message->text == '/countdown'  || '/countdown@RospoOV_Bot')
+    else if($update->message->text == '/countdown'  || $update->message->text == '/countdown@RospoOV_Bot')
     {
         $event = mktime(0,0,0,5,24,2016);
         $secondsLeft = $event - time();
