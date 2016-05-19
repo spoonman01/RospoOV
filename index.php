0576893a166f52b765a6bb939dc571ba1ffe70a7
spoonman01 <luca.rospocher@studenti.unitn.it>
2016-05-19 16:14:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -49,8 +49,8 @@ try {
         $secondsLeft = $event - time();
         
         $days = floor($secondsLeft / (60*60*24));
-        $hours = floor(($secondsLeft - $days*60*60*24) / (60*60))
-        $minutes = floor(($secondsLeft - $days*60*60*24 - $hours*60*60) / 60)
+        //$hours = floor(($secondsLeft - $days*60*60*24) / (60*60))
+        //$minutes = floor(($secondsLeft - $days*60*60*24 - $hours*60*60) / 60)
         
     	$response = $client->sendMessage([
          	'chat_id' => $update->message->chat->id,
